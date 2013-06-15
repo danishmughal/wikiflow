b0114368bc0b69542800e1f8c039a1b778dbbb9e
Robert Do <robertdo@robertdo-macbookpro.local>
2013-06-15 16:27:59 -0700

    search results page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Wikiflow::Application.routes.draw do
 
   root to: 'Main#home'
+
+  match "/search" => "main#search"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
